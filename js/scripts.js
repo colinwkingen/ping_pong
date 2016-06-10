7d097b1a99f8b0bc4ca2a1a0df67c64f8ee98846
Colin Kingen <colinwkingen@gmail.com>
2016-06-10 11:07:14 -0700

    add function to throw an error at non-numeric input and format returned values to replace old values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 var isNumber = true;
 
 var processInput = function(input) {
+  if (isNaN(parseInt(input)) === true) {
+    isNumber = false;
+    return;
+  };
   var number = parseInt(input);
   return number;
 };
 
 var processNumber = function(number) {
+  if (isNumber === false ) {
+    isNumber = true;
+    return "<span class='error word'> Thats not a number! </span>"
+  }
   var listStack = []
   for (i = number; i > 0; i -= 1) {
     if (i % 3 === 0) {
@@ -27,6 +35,6 @@ $(document).ready(function(){
     var inputData = $("#userInput").val();
     var inputNumber = processInput(inputData);
     var finalOutput = processNumber(inputNumber);
-    $(".output ul").text(finalOutput);
+    $(".output ul").html(finalOutput);
   });
 });
