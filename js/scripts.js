6f7c0cdc324b897e1dfdeccc789d82b9ac4194a2
Colin Kingen <colinwkingen@gmail.com>
2016-06-10 09:42:34 -0700

    add basic logic to return numbers and text if an ideal input is provided.

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+var isNumber = true;
+
+var processInput = function(input) {
+  var number = parseInt(input);
+  return number;
+};
+
+var processNumber = function(number) {
+  var listStack = []
+  for (i = number; i > 0; i -= 1) {
+    if (i % 3 === 0) {
+      listStack.push("<li> PING! </li>");
+    } else if (i % 5 === 0) {
+      listStack.push("<li> PONG </li>");
+    } else {
+      listStack.push("<li> " + i +" </li>")
+    };
+  };
+  return listStack;
+};
+
+
+
+$(document).ready(function(){
+  $("form").submit(function(event) {
+    event.preventDefault()
+    var inputData = $("#userInput").val();
+    var inputNumber = processInput(inputData);
+    var finalOutput = processNumber(inputNumber);
+    $(".output ul").text(finalOutput);
+  });
+});
